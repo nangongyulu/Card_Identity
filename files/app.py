69b9ebc8c6038cf48e20f529c043a5e476fc6815
Sleepywade <2129230438@qq.com>
2022-08-19 15:44:11 +0800

    flask界面美化

--- files/app.py
+++ files/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from flask import render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request
 import os
 import time
 
-# 创建项目
+# 设置文件类型
 app = Flask(__name__)
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 # 全局变量 共享的文件夹路径 可以根据需求更改
 DIRECTORY_FOLDER = 'static'
@@ -36,14 +35,12 @@ def get_files_data():
 
 
 @app.route("/")
-def index():
-    """共享文件主页"""
+def index():  # 主页
     return render_template("index.html", files=get_files_data())
 
 
 @app.route("/download_file/<filename>")
 def file_content(filename):
-    """下载文件的URL"""
     if filename in os.listdir(DIRECTORY_PATH):  # 如果需求下载文件存在
         # 发送文件 参数：文件夹路径，文件路径，文件名
         return send_from_directory(DIRECTORY_PATH, DIRECTORY_PATH + "/" + filename, filename)
@@ -54,19 +51,19 @@ def file_content(filename):
 
 @app.route("/upload_file", methods=['GET', 'POST'])
 def upload():
-    """上传文件的URL 支持GET/POST请求"""
     if request.method == "POST":
         # 获取文件 拼接存储路径并保存
         upload_file = request.files.get("upload_file")
         upload_file.save(os.path.join(DIRECTORY_PATH, upload_file.filename))
-
         #  返回上传成功的模板
         return render_template("upload_ok.html", filename=upload_file.filename)
-
     # 上传的网页
-
     return render_template("upload.html")
 
+
+# @app.route("/identity_file")
+# def():
+#     
 
 if __name__ == '__main__':
     app.run()
