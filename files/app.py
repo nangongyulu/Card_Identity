36f856478599d076d610976ef0817125fb51e482
Sleepywade <2129230438@qq.com>
2022-08-20 14:50:46 +0800

    初步做出整体项目

--- files/app.py
+++ files/app.py
@@ -1,16 +1,21 @@
-from flask import Flask
-from flask import render_template, send_from_directory, request
+from flask import Flask, render_template, request, make_response
 import os
+import config
 import time
 
-# 创建项目
+# 设置文件类型
 app = Flask(__name__)
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config.from_object(config)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 # 全局变量 共享的文件夹路径 可以根据需求更改
-DIRECTORY_FOLDER = 'static'
+DIRECTORY_FOLDER = 'static/'
 # 上传的文件夹路径
 DIRECTORY_PATH = os.path.join(app.root_path, 'static')
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rspliit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 # 获取文件信息的函数
@@ -34,41 +39,23 @@ def get_files_data():
         })
     return files
 
+
 @app.route("/")
-def HomePage():
-    return render_template("HomePage.html")
-
-@app.route("/index")
-def index():
-    """共享文件主页"""
+def index():  # 主页
     return render_template("index.html", files=get_files_data())
-
-
-@app.route("/download_file/<filename>")
-def file_content(filename):
-    """下载文件的URL"""
-    if filename in os.listdir(DIRECTORY_PATH):  # 如果需求下载文件存在
-        # 发送文件 参数：文件夹路径，文件路径，文件名
-        return send_from_directory(DIRECTORY_PATH, DIRECTORY_PATH + "/" + filename, filename)
-    else:
-        # 否则返回错误页面
-        return render_template("download_error.html", filename=filename)
 
 
 @app.route("/upload_file", methods=['GET', 'POST'])
 def upload():
-    """上传文件的URL 支持GET/POST请求"""
     if request.method == "POST":
         # 获取文件 拼接存储路径并保存
         upload_file = request.files.get("upload_file")
         upload_file.save(os.path.join(DIRECTORY_PATH, upload_file.filename))
-
         #  返回上传成功的模板
         return render_template("upload_ok.html", filename=upload_file.filename)
-
     # 上传的网页
-
     return render_template("upload.html")
+
 
 if __name__ == '__main__':
     app.run()
