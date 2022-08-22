1d480b144e1e2c41b8b4d5c2f4597047633f1e04
Sleepywade <2129230438@qq.com>
2022-08-22 08:41:36 +0800

    代码优化

--- files/app.py
+++ files/app.py
@@ -9,9 +9,10 @@ app.config.from_object(config)
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 # 全局变量 共享的文件夹路径 可以根据需求更改
-DIRECTORY_FOLDER = 'image'
+UPLOAD_FOLDER = 'image'
 # 上传的文件夹路径
-DIRECTORY_PATH = os.path.join(app.root_path, 'image')
+UPLOAD_PATH = os.path.join(app.root_path, 'image')
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rspliit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -20,12 +21,12 @@ def allowed_file(filename):
 # 获取文件信息的函数
 def get_files_data():
     files = []
-    for i in os.listdir(DIRECTORY_PATH):
+    for i in os.listdir(UPLOAD_PATH):
         if len(i.split(".")) == 1:  # 判断此文件是否为一个文件夹
             continue
 
         # 拼接路径
-        file_path = DIRECTORY_PATH + "/" + i
+        file_path = UPLOAD_PATH + "/" + i
         name = i
         size = os.path.getsize(file_path)  # 获取文件大小
         ctime = time.localtime(os.path.getctime(file_path))  # 格式化创建当时的时间戳
@@ -38,9 +39,11 @@ def get_files_data():
         })
     return files
 
+
 @app.route("/")
 def HomePage():
     return render_template("HomePage.html")
+
 
 @app.route("/index")
 def index():
@@ -54,14 +57,29 @@ def upload():
     if request.method == "POST":
         # 获取文件 拼接存储路径并保存
         upload_file = request.files.get("upload_file")
-        upload_file.save(os.path.join(DIRECTORY_PATH, upload_file.filename))
-
+        upload_file.save(os.path.join(UPLOAD_PATH, upload_file.filename))
         #  返回上传成功的模板
         return render_template("upload_ok.html", filename=upload_file.filename)
 
     # 上传的网页
 
     return render_template("upload.html")
+
+
+@app.route('/show/<string:filename>', methods=['GET'])
+def show_photo(filename):
+    file_dir = os.path.join(UPLOAD_PATH, app.config['UPLOAD_FOLDER'])
+    if request.method == 'GET':
+        if filename is None:
+            pass
+        else:
+            image_data = open(os.path.join(file_dir, '%s' % filename), "rb").read()
+            response = make_response(image_data)
+            response.headers['Content-Type'] = ''
+            return response
+    else:
+        pass
+
 
 if __name__ == '__main__':
     app.run()
