f415dd87b12e2d1fee31ff883b9d79ed1dcbea22
Sleepywade <2129230438@qq.com>
2022-08-24 09:12:54 +0800

    整体项目

---
+++ Code_body/forward.py
@@ -0,0 +1,36 @@
+# import tensorflow.compat.v1 as tf
+import tensorflow._api.v2.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+INPUT_NODE = 256  # 输入层
+OUTPUT_NODE = 10  # 输出层
+LAYER1_NODE = 100  # 隐藏层
+LAYER2_NODE = 100  # 隐藏层2
+
+
+def get_weight(shape, regularizer):
+    w = tf.Variable(tf.compat.v1.random.truncated_normal(shape, stddev=0.1))
+    if regularizer is not None:
+        tf.add_to_collection('losses', tf.keras.regularizers.l2(regularizer)(w))
+    return w
+
+
+def get_bias(shape):
+    b = tf.Variable(tf.zeros(shape))
+    return b
+
+
+def forward(x, regularizer):
+    w1 = get_weight([INPUT_NODE, LAYER1_NODE], regularizer)
+    b1 = get_bias([LAYER1_NODE])
+    y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+
+    w2 = get_weight([LAYER1_NODE, LAYER2_NODE], regularizer)
+    b2 = get_bias([LAYER2_NODE])
+    y2 = tf.nn.relu(tf.matmul(y1, w2) + b2)
+
+    w3 = get_weight([LAYER2_NODE, OUTPUT_NODE], regularizer)
+    b3 = get_bias([OUTPUT_NODE])
+    y = tf.matmul(y2, w3) + b3
+    return y
